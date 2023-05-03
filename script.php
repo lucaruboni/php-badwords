dbcaec66a42e6a54979644fa2c5278ce298d4acb
lucaruboni <lucaruboni@gmail.com>
2023-05-03 17:55:15 +0200

    [add] show on screen paragraphe and badword

--- script.php
+++ script.php
@@ -2,8 +2,47 @@
 
 
 
-var_dump($_POST);
-echo $paragraphe;
-echo $badword;
-$paragraphe = $argv[1];
-$badword = $argv[2];
+
+
+
+echo $_POST['paragraphe'];
+echo $_POST['badword'];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+</head>
+<body>
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <h1 class="text-center">
+            <?php
+              
+              echo $_POST['paragraphe']
+
+            ?>
+        </h1>
+
+        <h1 class="text-center">
+            <?php
+              
+              echo $_POST['badword']
+              
+            ?>
+        </h1>
+    </div>
+</div>
+
+    
+</body>
+</html>
